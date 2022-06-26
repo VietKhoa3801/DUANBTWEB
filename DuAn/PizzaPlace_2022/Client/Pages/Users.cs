deb9096f6e609d9e50d3a805d0d5409677e28281
AnhKhoa0308 <you@example.com>
2022-06-26 23:50:03 +0700

    first commit

---
+++ DuAn/PizzaPlace_2022/Client/Pages/Users.cs
@@ -0,0 +1,12 @@
+﻿namespace PizzaPlace_2022.Client.Pages
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+    }
+}
+
